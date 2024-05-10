a9a6b4fc1d13c2e2d0f30c0051e535c77d7551d3
kikhint <kikhint@yandex.ru>
2024-05-10 15:02:42 +0300

    edited the main page

--- htdocs/blocks/header.php
+++ htdocs/blocks/header.php
@@ -17,6 +17,9 @@
     <div class="container d-flex flex-wrap justify-content-center">
         <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
             <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mask" viewBox="0 0 16 16">
+                <path d="M6.225 1.227A7.5 7.5 0 0 1 10.5 8a7.5 7.5 0 0 1-4.275 6.773 7 7 0 1 0 0-13.546M4.187.966a8 8 0 1 1 7.627 14.069A8 8 0 0 1 4.186.964z"/>
+            </svg>
             <span class="fs-4"><ya-tr-span data-index="20-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Double header" data-translation="Двойной заголовок" data-ch="0" data-type="trSpan" style="visibility: initial !important;" data-selected="false">Кубрик</ya-tr-span></span>
         </a>
         <form class="col-12 col-lg-auto mb-3 mb-lg-0" role="search">
--- htdocs/index.php
+++ htdocs/index.php
@@ -13,54 +13,57 @@
     <?php require "blocks/header.php" ?>
     <div class="container mt-5">
         <h4 class="mb-5">Что посмотреть на майские</h4>
-        <div class="d-flex flex-wrap">
-            <div class="card mb-4 rounded-3 shadow-sm">
-                <div class="card-header py-3">
-                    <h4 class="my-0 fw-normal"><ya-tr-span data-index="8-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Free" data-translation="Бесплатно" data-ch="0" data-type="trSpan" style="visibility: initial !important;">ен</ya-tr-span></h4>
-                </div>
-                <div class="card-body">
-                    <img src="img/.jpg" class="img-thumbnail">
-                    <ul class="list-unstyled mt-3 mb-4">
-                        <li><ya-tr-span data-index="10-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="10 users included" data-translation="включено 10 пользователей" data-ch="0" data-type="trSpan" style="visibility: initial !important;">включено 10</ya-tr-span></li>
-                        <li><ya-tr-span data-index="11-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="2 GB of storage" data-translation="2 ГБ памяти" data-ch="0" data-type="trSpan" style="visibility: initial !important;">2 ГБ памяти</ya-tr-span></li>
-                        <li><ya-tr-span data-index="12-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Email support" data-translation="Поддержка по электронной почте" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Поддержка по электронной почте</ya-tr-span></li>
-                        <li><ya-tr-span data-index="13-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Help center access" data-translation="Доступ к справочному центру" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Доступ к справочному центру</ya-tr-span></li>
-                    </ul>
-                    <button type="button" class="w-100 btn btn-lg btn-outline-primary"><ya-tr-span data-index="14-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Sign up for free" data-translation="Зарегистрируйтесь бесплатно" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Подробнее</ya-tr-span></button>
-                </div>
-            </div>
-        </div>
-        <div class="d-flex flex-wrap">
-            <div class="card mb-4 rounded-3 shadow-sm">
-                <div class="card-header py-3">
-                    <h4 class="my-0 fw-normal"><ya-tr-span data-index="8-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Free" data-translation="Бесплатно" data-ch="0" data-type="trSpan" style="visibility: initial !important;">ен</ya-tr-span></h4>
-                </div>
-                <div class="card-body">
-                    <img src="img/.jpg" class="img-thumbnail">
-                    <ul class="list-unstyled mt-3 mb-4">
-                        <li><ya-tr-span data-index="10-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="10 users included" data-translation="включено 10 пользователей" data-ch="0" data-type="trSpan" style="visibility: initial !important;">включено 10</ya-tr-span></li>
-                        <li><ya-tr-span data-index="11-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="2 GB of storage" data-translation="2 ГБ памяти" data-ch="0" data-type="trSpan" style="visibility: initial !important;">2 ГБ памяти</ya-tr-span></li>
-                        <li><ya-tr-span data-index="12-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Email support" data-translation="Поддержка по электронной почте" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Поддержка по электронной почте</ya-tr-span></li>
-                        <li><ya-tr-span data-index="13-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Help center access" data-translation="Доступ к справочному центру" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Доступ к справочному центру</ya-tr-span></li>
-                    </ul>
-                    <button type="button" class="w-100 btn btn-lg btn-outline-primary"><ya-tr-span data-index="14-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Sign up for free" data-translation="Зарегистрируйтесь бесплатно" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Подробнее</ya-tr-span></button>
+        <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm">
+                    <div class="card-header py-3">
+                        <h4 class="my-0 fw-normal"><ya-tr-span data-index="7-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Free" data-translation="Бесплатно" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Годзилла: Минус один (2023)</ya-tr-span></h4>
+                    </div>
+                    <div class="card-body">
+                        <img src="img/1.jpg" class="img-thumbnail">
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li><ya-tr-span data-index="9-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="10 users included" data-translation="включено 10 пользователей" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Год производства - 2023</ya-tr-span></li>
+                            <li><ya-tr-span data-index="10-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="2 GB of storage" data-translation="2 ГБ памяти" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Страна - Япония</ya-tr-span></li>
+                            <li><ya-tr-span data-index="11-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Email support" data-translation="Поддержка по электронной почте" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Жанр - фантастика, боевик, драма</ya-tr-span></li>
+                            <li><ya-tr-span data-index="12-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Help center access" data-translation="Доступ к справочному центру" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Режиссер - Такаси Ямадзаки</ya-tr-span></li>
+                        </ul>
+                        <button type="button" class="w-100 btn btn-lg btn-outline-primary"><ya-tr-span data-index="13-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Sign up for free" data-translation="Зарегистрируйтесь бесплатно" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Подробнее</ya-tr-span></button>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="d-flex flex-wrap">
-            <div class="card mb-4 rounded-3 shadow-sm">
-                <div class="card-header py-3">
-                    <h4 class="my-0 fw-normal"><ya-tr-span data-index="8-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Free" data-translation="Бесплатно" data-ch="0" data-type="trSpan" style="visibility: initial !important;">ен</ya-tr-span></h4>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm">
+                    <div class="card-header py-3">
+                        <h4 class="my-0 fw-normal"><ya-tr-span data-index="14-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Pro" data-translation="Профессиональный" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Леди Баг и Супер-Кот: Пробуждение силы (2023)</ya-tr-span></h4>
+                    </div>
+                    <div class="card-body">
+                        <img src="img/2.jpg" class="img-thumbnail">
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li><ya-tr-span data-index="16-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="20 users included" data-translation="включено 20 пользователей" data-ch="0" data-type="trSpan" style="visibility: initial !important;" data-selected="false">Год производства - 2023</ya-tr-span></li>
+                            <li><ya-tr-span data-index="17-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="10 GB of storage" data-translation="10 ГБ памяти" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Страна - Франция</ya-tr-span></li>
+                            <li><ya-tr-span data-index="18-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Priority email support" data-translation="Приоритетная поддержка по электронной почте" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Жанр - мультфильм, семейный, детский, фэнтези, мелодрама, комедия, приключения</ya-tr-span></li>
+                            <li>Режиссер - Джереми Заг</li>
+                        </ul>
+                        <button type="button" class="w-100 btn btn-lg btn-outline-primary"><ya-tr-span data-index="13-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Sign up for free" data-translation="Зарегистрируйтесь бесплатно" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Подробнее</ya-tr-span></button>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <img src="img/.jpg" class="img-thumbnail">
-                    <ul class="list-unstyled mt-3 mb-4">
-                        <li><ya-tr-span data-index="10-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="10 users included" data-translation="включено 10 пользователей" data-ch="0" data-type="trSpan" style="visibility: initial !important;">включено 10</ya-tr-span></li>
-                        <li><ya-tr-span data-index="11-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="2 GB of storage" data-translation="2 ГБ памяти" data-ch="0" data-type="trSpan" style="visibility: initial !important;">2 ГБ памяти</ya-tr-span></li>
-                        <li><ya-tr-span data-index="12-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Email support" data-translation="Поддержка по электронной почте" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Поддержка по электронной почте</ya-tr-span></li>
-                        <li><ya-tr-span data-index="13-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Help center access" data-translation="Доступ к справочному центру" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Доступ к справочному центру</ya-tr-span></li>
-                    </ul>
-                    <button type="button" class="w-100 btn btn-lg btn-outline-primary"><ya-tr-span data-index="14-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Sign up for free" data-translation="Зарегистрируйтесь бесплатно" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Подробнее</ya-tr-span></button>
+            </div>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm">
+                    <div class="card-header py-3">
+                        <h4 class="my-0 fw-normal">Дюна: Часть вторая (2024)</h4>
+                    </div>
+                    <div class="card-body">
+                        <img src="img/4.jpg" class="img-thumbnail">
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li><ya-tr-span data-index="21-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="30 users included" data-translation="включено 30 пользователей" data-ch="0" data-type="trSpan" style="visibility: initial !important;"></ya-tr-span></li>
+                            <li><ya-tr-span data-index="22-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="15 GB of storage" data-translation="15 ГБ памяти" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Год производства - 2024</ya-tr-span></li>
+                            <li><ya-tr-span data-index="23-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Phone and email support" data-translation="Поддержка по телефону и электронной почте" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Страна - США, Канада, ОАЭ, Венгрия, Италия, Новая Зеландия, Иордания, Гамбия</ya-tr-span></li>
+                            <li>Жанр - фантастика, боевик, драма, приключения</li>
+                            <li>Режиссер - Дени Вильнёв</li>
+                        </ul>
+                        <button type="button" class="w-100 btn btn-lg btn-outline-primary"><ya-tr-span data-index="13-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Sign up for free" data-translation="Зарегистрируйтесь бесплатно" data-ch="0" data-type="trSpan" style="visibility: initial !important;">Подробнее</ya-tr-span></button>
+                    </div>
                 </div>
             </div>
         </div>
